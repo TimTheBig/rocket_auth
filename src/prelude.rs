2abf01a39efcef13578d841613148b61f649280f
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2024-09-03 08:53:59 -0400

    Fix dependences

--- src/prelude.rs
+++ src/prelude.rs
@@ -9,7 +9,7 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 pub(crate) use crate::db::DBConnection;
 pub(crate) use crate::session::SessionManager;
 pub(crate) use async_trait::async_trait;
-pub(crate) use fehler::*;
+pub(crate) use culpa::*;
 pub(crate) use rocket::form::FromForm;
 pub(crate) use serde::{Deserialize, Serialize};
 pub(crate) use std::fmt::Debug;
